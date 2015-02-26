90d18fedea94211fd9131f60b3b355c9c9ddbc19
Wade Womersley <wade85@gmail.com>
2015-02-26 09:57:34 +0000

    Improved nuget build and targetted 4.0 and 4.5

---
+++ build.bat
@@ -0,0 +1,2 @@
+msbuild xcitestudios-parallelisation.csproj /t:Build;Package /p:Configuration="Release 4.5"
+msbuild xcitestudios-parallelisation.csproj /t:Build;Package /p:Configuration="Release 4.0"
