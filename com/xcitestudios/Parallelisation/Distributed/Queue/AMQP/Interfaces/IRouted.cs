3a6bf1862fc4097194489a683f38b31a65a4e2d8
Wade Womersley <wade85@gmail.com>
2015-03-15 22:27:21 +0000

    Start of AMQP - will fail build.

---
+++ com/xcitestudios/Parallelisation/Distributed/Queue/AMQP/Interfaces/IRouted.cs
@@ -0,0 +1,35 @@
+﻿namespace com.xcitestudios.Parallelisation.Distributed.Queue.AMQP.Interfaces
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using com.xcitestudios.Parallelisation.Interfaces;
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <typeparam name="T">IEvent</typeparam>
+    /// <typeparam name="U">IEventInput</typeparam>
+    /// <typeparam name="V">IEventOutput</typeparam>
+    public interface IRouted<T, U, V> : IEventHandler<T, U, V>
+        where T: IEvent<U, V>
+        where U: IEventInput
+        where V: IEventOutput
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        string RoutingKey { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        string ReturnQueue { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        string CorrelationID { get; set; }
+    }
+}
